7cc818cdc97376240b71ae43ee30de4adc5477ea
Ian Preston <17241371+ianepreston@users.noreply.github.com>
2019-12-02 07:43:28 -0700

    finished part 2

--- advent_2019/day_02/opcode.py
+++ advent_2019/day_02/opcode.py
@@ -3,12 +3,11 @@ def readlist(file="day_02/input.txt"):
         return [int(char) for char in f.readline().split(",")]
 
 
-def opcoder(in_list, replacement=False):
+def opcoder(in_list, replacement=None):
     i = 0
     work_list = in_list[:]
-    if replacement:
-        work_list[1] = 12
-        work_list[2] = 2
+    if replacement is not None:
+        work_list[1], work_list[2] = replacement
     while work_list[i] != 99:
         j = work_list[i + 1]
         k = work_list[i + 2]
@@ -28,4 +27,16 @@ assert opcoder([2, 3, 0, 3, 99]) == [2, 3, 0, 6, 99]
 assert opcoder([2, 4, 4, 5, 99, 0]) == [2, 4, 4, 5, 99, 9801]
 assert opcoder([1, 1, 1, 4, 99, 5, 6, 0, 99]) == [30, 1, 1, 4, 2, 5, 6, 0, 99]
 
-print(opcoder(readlist(), replacement=True)[0])
+print(opcoder(readlist(), replacement=(12, 2))[0])
+
+
+def opcoder2():
+    base = readlist()
+    for noun in range(100):
+        for verb in range(100):
+            if opcoder(base, replacement=(noun, verb))[0] == 19690720:
+                return 100 * noun + verb
+
+
+print(opcoder2())
+
