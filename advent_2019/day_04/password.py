5d83cd4fc470b25a4ae045f0ccb8fc8d25465ebb
Ian Preston <17241371+ianepreston@users.noreply.github.com>
2019-12-04 09:32:14 -0700

    finished day 4

--- advent_2019/day_04/password.py
+++ advent_2019/day_04/password.py
@@ -1,3 +1,6 @@
+from collections import Counter
+
+
 def digits(num):
     return tuple(map(int, str(num)))
 
@@ -19,5 +22,25 @@ assert validator(111111)
 assert ~validator(223450)
 assert ~validator(123789)
 
-passes = [x for x in range(172_930, 683083) if validator(x)]
+passes = [x for x in range(172_930, 683_083) if validator(x)]
+print(len(passes))
+
+
+def validator2(num):
+    nums = digits(num)
+    adjacent = False
+    monotonic = True
+    for i, dig in enumerate(nums):
+        if any(dig > y for y in nums[i + 1 :]):
+            monotonic = False
+    if any(val == 2 for val in Counter(nums).values()):
+        adjacent = True
+    return adjacent & monotonic
+
+
+assert validator2(112233)
+assert ~validator2(123444)
+assert validator2(111122)
+
+passes = [x for x in range(172_930, 683_083) if validator2(x)]
 print(len(passes))
