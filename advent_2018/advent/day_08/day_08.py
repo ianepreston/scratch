db40968e084407290aa3f54f0d537399e2bc23bd
Ian Preston <17241371+ianepreston@users.noreply.github.com>
2019-12-03 17:56:29 -0700

    try lunch and learn implementation

--- advent_2018/advent/day_08/day_08.py
+++ advent_2018/advent/day_08/day_08.py
@@ -1,8 +1,32 @@
+EX = "advent/day_08/example.txt"
+IN = "advent/day_08/input.txt"
+
 def treelist(file):
     with open(file, "r") as f:
         line = f.readline()
         return [int(char) for char in line.split()]
 
+def lunch_method(file):
+    data = treelist(file)
+    i = 0
+    j = len(data)
+    total = list()
+    while i != j:
+        if data[i] == 0:
+            i += 1
+            for x in range(i, i + data[i] + 1):
+                total.append(data[x])
+            i += data[i]
+        else:
+            i += 1
+            for x in range(j - data[i] + 1, j):
+                total.append(data[x])
+            j = j - data[i]
+    return total
+
+
+# assert lunch_method(EX) == 138
+    
 
 class Node:
     def __init__(self):
@@ -16,4 +40,3 @@ class Node:
             self.n_children = num
         elif self.n_meta is None:
             self.n_meta = num
-        elif 
