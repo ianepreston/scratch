b1c6ab835ec9a18db84c6dd12672c4e36b04b8cd
[Ian Preston] <17241371+ianepreston@users.noreply.github.com>
2019-12-10 16:39:10 -0700

    little more progress

--- advent_2019/day_10/asteroids.py
+++ advent_2019/day_10/asteroids.py
@@ -28,5 +28,34 @@ class Line:
         self.point2 = point2
     
     @property
-    def slope(self):
-        pass
+    def min_x(self):
+        return min(self.point1.x, self.point2.x)
+    
+    @property
+    def max_x(self):
+        return max(self.point1.x, self.point2.x)
+    
+
+    @property
+    def min_y(self):
+        return min(self.point1.y, self.point2.y)
+    
+    @property
+    def max_y(self):
+        return max(self.point1.y, self.point2.y)
+
+    
+    def point_in_box(self, point):
+        in_x = self.min_x <= point.x <= self.max_x
+        in_y = self.min_y <= point.y <= self.max_y
+        return in_x & in_y
+    
+    def point_on_line(self, point):
+        if ~self.point_in_box(point):
+            return False
+        if self.point1.x == self.point2.x:
+            return self.min_y < point.y < self.max_y
+        if self.point1.y == self.point2.y:
+            return self.min_x < point.x < self.max_x
+        
+
