8716ea3427e376d3c20bccbb4a800143d98a88e4
Ian Preston <17241371+ianepreston@users.noreply.github.com>
2019-12-15 00:52:43 -0700

    build and start testing intcode computer

--- advent_2019/adventlib/adventop.py
+++ advent_2019/adventlib/adventop.py
@@ -1,4 +1,105 @@
 """Op code manager for advent of code"""
+from pathlib import Path
+
+class IntCode:
+    """Might as well build this right finally"""
+    def __init__(self, prog):
+        if isinstance(prog, str) or isinstance(prog, Path):
+            with open(prog, "r") as f:
+                self.input_prog = [int(x) for x in f.readline().rstrip().split(",")]
+        else:
+            self.input_prog = prog
+        self.work_prog = self.input_prog[:] # make a copy so we can restart if necessary
+        self.index = 0
+        self.inputs = list()
+        self.outputs = list()
+
+    
+    @staticmethod
+    def parse_instruction(instruction):
+        sint = str(instruction).zfill(5)
+        parm3 = bool(int(sint[0]))
+        parm2 = bool(int(sint[1]))
+        parm1 = bool(int(sint[2]))
+        op = int(sint[-2:])
+        return op, parm1, parm2, parm3
+    
+    def parnum(self, parm, num):
+        """Depending on parameter mode return an index or a number"""
+        if parm:
+            return num
+        else:
+            return self.work_prog[num]
+    
+    def execute_op(self):
+        op, parm1, parm2, parm3 = IntCode.parse_instruction(self.work_prog[self.index])
+        j = self.parnum(parm1, self.index + 1)
+        k = self.parnum(parm2, self.index + 2)
+        m = self.parnum(parm3, self.index + 3)
+        if op == 1:
+            self.work_prog[m] = self.work_prog[j] + self.work_prog[k]
+            self.index += 4
+            return (True, None)
+        elif op == 2:
+            self.work_prog[m] = self.work_prog[j] * self.work_prog[k]
+            self.index += 4
+            return (True, None)
+        elif op == 3:
+            self.work_prog[j] = self.inputs.pop(0)
+            self.index += 2
+            return (True, None)
+        elif op == 4:
+            self.outputs.append(self.work_prog[j])
+            self.index += 2
+            return (True, self.work_prog[j])
+        elif op == 5:
+            if self.work_prog[j] != 0:
+                self.index = self.work_prog[k]
+            else:
+                self.index += 3
+            return (True, None)
+        elif op == 6:
+            if self.work_prog[j] == 0:
+                self.index = self.work_prog[k]
+            else:
+                self.index += 3
+            return (True, None)
+        elif op == 7:
+            if self.work_prog[j] < self.work_prog[k]:
+                self.work_prog[m] = 1
+            else:
+                self.work_prog[m] = 0
+            self.index += 4
+            return (True, None)
+        elif op == 8:
+            if self.work_prog[j] == self.work_prog[k]:
+                self.work_prog[m] = 1
+            else:
+                self.work_prog[m] = 0
+            self.index += 4
+        elif self.work_prog[self.index] == 99:
+            return (False, None)
+        else:
+            raise ValueError
+    
+    def receive_input(self, *args):
+        for arg in args:
+            self.inputs.append(arg)
+        return True
+    
+    def next_output(self):
+        while self.work_prog[self.index] != 99:
+            passfail, result = self.execute_op()
+            if result is not None:
+                return result
+    
+    def run_to_completion(self):
+        while self.work_prog[self.index] != 99:
+            self.execute_op()
+        return self.outputs
+
+
+
 def read_program(file):
     with open(file, "r") as f:
         return [int(x) for x in f.readline().rstrip().split(",")]
---
+++ advent_2019/adventlib/tests/test_adventlib.py
@@ -0,0 +1,42 @@
+from pathlib import Path
+import sys
+libpath = Path(__file__).parent.parent
+sys.path.append(str(libpath))
+from adventop import IntCode
+
+base = libpath.parent
+
+def test_day2_1():
+    inprog = [1,0,0,0,99]
+    compy = IntCode(inprog)
+    compy.run_to_completion()
+    assert compy.work_prog == [2,0,0,0,99]
+
+
+def test_day2_2():
+    inprog = [2,3,0,3,99]
+    compy = IntCode(inprog)
+    compy.run_to_completion()
+    assert compy.work_prog == [2,3,0,6,99]
+
+def test_day2_3():
+    inprog = base / "day_02" / "input.txt"
+    compy = IntCode(inprog)
+    compy.work_prog[1] = 12
+    compy.work_prog[2] = 2
+    compy.run_to_completion()
+    assert compy.work_prog[0] == 2782414
+
+def test_day2_4():
+    inprog = base / "day_02" / "input.txt"
+    def result_checker():
+        for noun in range(100):
+            for verb in range(100):
+                compy = IntCode(inprog)
+                compy.work_prog[1] = noun
+                compy.work_prog[2] = verb
+                compy.run_to_completion()
+                if compy.work_prog[0] == 19690720:
+                    return 100 * noun + verb
+    assert result_checker() == 9820
+
