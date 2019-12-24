f36d813a9a72c90daaa1bcb6ba0b999bd3a3cb84
Ian Preston <17241371+ianepreston@users.noreply.github.com>
2019-12-24 12:18:27 -0700

    rename

--- advent_2019/day_10/asteroids.py
+++ advent_2019/day_10/asteroids.py
@@ -1,14 +1,10 @@
 from pathlib import Path
-import sys
-
-here = Path(__file__).parent
-base = here.parent / "adventlib"
-sys.path.append(str(base))
-
-from collections import namedtuple, Counter
+from collections import namedtuple, defaultdict
+from math import atan2, dist, pi
+from fractions import Fraction
 
 Point = namedtuple("Point", ["x", "y"])
-LineEQ = namedtuple("LineEQ", ["m", "b"])
+here = Path(__file__).parent
 
 
 def read_points(file):
@@ -26,88 +22,76 @@ def read_points(file):
     return points
 
 
-def calc_slope(point1, point2):
-    if point1.x == point2.x:
-        return None
-    else:
-        m = (point1.y - point2.y) / (point1.x - point2.x)
-        b = -(m * point1.x - point1.y)
-        return LineEQ(m, b)
+def relative_position(source, destination):
+    """return a point showing the x, y of destination relative to source
+    e.g. if source is at (2, 4) and destination is at (3, 5) this will
+    return (1, -1)
+    I reverse on the y axis because the stupid coordinate system is negative in that direction
+    """
+    return Point(destination.x - source.x, source.y - destination.y)
 
 
-class Line:
-    def __init__(self, point1, point2):
-        self.point1 = point1
-        self.point2 = point2
-
-    @property
-    def min_x(self):
-        return min(self.point1.x, self.point2.x)
-
-    @property
-    def max_x(self):
-        return max(self.point1.x, self.point2.x)
-
-    @property
-    def min_y(self):
-        return min(self.point1.y, self.point2.y)
-
-    @property
-    def max_y(self):
-        return max(self.point1.y, self.point2.y)
-
-    def point_in_box(self, point):
-        in_x = self.min_x <= point.x <= self.max_x
-        in_y = self.min_y <= point.y <= self.max_y
-        return in_x & in_y
-
-    def point_on_line(self, point):
-        if not self.point_in_box(point):
-            return False
-        elif self.point1.x == self.point2.x:
-            return self.min_y < point.y < self.max_y
-        elif self.point1.y == self.point2.y:
-            return self.min_x < point.x < self.max_x
-        else:
-            return calc_slope(self.point1, self.point2) == calc_slope(
-                self.point1, point
-            )
+def calc_radians(source, destination):
+    """Not quite radians, but close enough"""
+    x, y = relative_position(source, destination)
+    rad = atan2(y, x)
+    if y >= 0 and x < 0:
+        rad = -(2 * pi) + rad
+    return rad
 
 
-test = Line(Point(3, 4), Point(1, 0))
-assert test.point_on_line(Point(2, 2))
-tp = Point(3, 4)
-test = Line(tp, tp)
-assert not test.point_on_line(Point(2, 2))
-assert not test.point_on_line(tp)
+class AsteroidField:
+    def __init__(self, file):
+        self.field = read_points(file)
+        self.min_x = min(point.x for point in self.field)
+        self.max_x = max(point.x for point in self.field)
+        self.min_y = min(point.y for point in self.field)
+        self.max_y = max(point.y for point in self.field)
+
+    def count_slopes(self, point):
+        return len(
+            set(calc_radians(point, dest) for dest in self.field if dest != point)
+        )
+
+    def rank_fields(self, point):
+        angle_dict = defaultdict(list)
+        for dest in self.field:
+            rads = calc_radians(point, dest)
+            angle_dict[rads].append(dest)
+        for points in angle_dict.values():
+            points.sort(key=lambda x: dist(point, x), reverse=True)
+        while angle_dict:
+            keys = angle_dict.keys()
+            keys = sorted(keys, reverse=True)
+            for key in keys:
+                rad_points = angle_dict[key]
+                yield rad_points.pop()
+                if not rad_points:
+                    del angle_dict[key]
+        
+
+    def best(self):
+        result = {point: self.count_slopes(point) for point in self.field}
+        max_point = max(result, key=lambda k: result[k])
+        return result[max_point], max_point
+
+    def part2(self):
+        _, base = self.best()
+        return [point for point in self.rank_fields(base)]
 
 
-def observable_points(points):
-    point_counter = Counter()
-    for source in points:
-        for dest in points.difference(set(source)):
-            line = Line(source, dest)
-            if all(
-                not line.point_on_line(point)
-                for point in points.difference(set((source, dest)))
-            ):
-                point_counter[source] += 1
-    return point_counter
+assert AsteroidField(here / "ex1.txt").best() == (8, Point(3, 4))
+assert AsteroidField(here / "ex2.txt").best() == (33, Point(5, 8))
+assert AsteroidField(here / "ex3.txt").best() == (35, Point(1, 2))
+assert AsteroidField(here / "ex4.txt").best() == (41, Point(6, 3))
+assert AsteroidField(here / "ex5.txt").best() == (210, Point(11, 13))
+assert AsteroidField(here / "input.txt").best() == (256, Point(29, 28))
 
+ex5_field = AsteroidField(here / "ex5.txt")
+ex5_ranks = ex5_field.part2()
+assert ex5_ranks[199] == Point(8,2)
 
-def best_asteroid(points):
-    point_counter = observable_points(points)
-    max_key = max(point_counter, key=lambda k: point_counter[k])
-    return (
-        point_counter[max_key] - 1,
-        max_key,
-    )  # not totally sure why I need to subtract one
-
-
-assert best_asteroid(read_points(here / "ex1.txt")) == (8, Point(3, 4))
-assert best_asteroid(read_points(here / "ex2.txt")) == (33, Point(5, 8))
-assert best_asteroid(read_points(here / "ex3.txt")) == (35, Point(1, 2))
-assert best_asteroid(read_points(here / "ex4.txt")) == (41, Point(6, 3))
-assert best_asteroid(read_points(here / "ex5.txt")) == (210, Point(11, 13))
-# print(best_asteroid(read_points(here / "input.txt")))
-assert best_asteroid(read_points(here/ "input.txt")) == (256, Point(29, 28))
+fin_field = AsteroidField(here / "input.txt")
+fin_point = fin_field.part2()[199]
+answer = fin_point.x * 100 + fin_point.y
+print(answer)
