1167d4c011cf7e04f98baef0796f54cfd5c4c058
Ian Preston <17241371+ianepreston@users.noreply.github.com>
2019-12-27 13:49:31 -0700

    cleaned up after part 1

--- advent_2019/day_14/compounds.py
+++ advent_2019/day_14/compounds.py
@@ -1,7 +1,7 @@
 from pathlib import Path
 import re
 import math
-from collections import OrderedDict, Counter
+from collections import Counter
 
 here = Path(__file__).parent.resolve()
 
@@ -13,10 +13,6 @@ EX5 = here / "ex5.txt"
 IN = here / "input.txt"
 
 ALL_IN = [EX1, EX2, EX3, EX4, EX5, IN]
-
-
-def ceildiv(a, b):
-    return -(-a // b)
 
 
 def parse_line(input_str):
@@ -41,28 +37,19 @@ class Compound:
 
     def __repr__(self):
         return self.name
-    
-    def calc_depth(self):
-        precursors = self.precursors
-        if len(precursors) == 1 and precursors[0][0].name == "ORE":
-            return 1
-        elif self.name == "ORE":
-            return 0
-        else:
-            return 1 + max(precursor[0].depth for precursor in precursors)
-    
-    @property
-    def depth(self):
-        if self._depth is None:
-            self._depth = self.calc_depth()
-        return self._depth
-    
+
     def reaction(self):
         ore_count = 0
         compound_count = Counter()
         compound_count[self] += 1
-        while any(val >0 for val in compound_count.values()):
-            compound = next(iter(compound for compound, numreq in compound_count.items() if numreq > 0))
+        while any(val > 0 for val in compound_count.values()):
+            compound = next(
+                iter(
+                    compound
+                    for compound, numreq in compound_count.items()
+                    if numreq > 0
+                )
+            )
             numreq = compound_count[compound]
             reactions = math.ceil(numreq / compound.outnum)
             compound_count[compound] -= compound.outnum * reactions
@@ -73,36 +60,6 @@ class Compound:
             else:
                 for compound, required in precursors:
                     compound_count[compound] += required * reactions
-        return ore_count
-            
-
-
-
-    def reaction_old(self):
-        ore_count = 0
-        compound_count = Counter()
-        compound_queue = OrderedDict()
-        compound_queue[self] = 1
-        while compound_queue:
-            compound_queue = OrderedDict(sorted(compound_queue.items(), key=lambda x: x[0].depth, reverse=True))
-            compound, numreq = compound_queue.popitem(last=False)
-            if numreq <= compound_count[compound]:
-                compound_count[compound] -= numreq
-            else:
-                precursors = compound.precursors
-                if len(precursors) == 1 and precursors[0][0].name == "ORE":
-                    ore, orenum = precursors[0]
-                    compout = compound.outnum
-                    reactions = ceildiv(numreq, compout)
-                    ore_count += orenum * reactions
-                    compound_count[compound] += (reactions * compout) -numreq
-                else:
-                    for compound, required in precursors:
-                        tomake = ceildiv(required * numreq, compound.outnum)
-                        if compound in compound_queue.keys():
-                            compound_queue[compound] += tomake 
-                        else:
-                            compound_queue[compound] = tomake 
         return ore_count
 
 
@@ -165,7 +122,4 @@ assert part1(EX2) == 165
 assert part1(EX3) == 13312
 assert part1(EX4) == 180697
 assert part1(EX5) == 2210736
-print(part1(IN))
-# compounds = parse_compounds(EX1)
-# compounds = OrderedDict(compounds)
-# ore = compounds["ORE"]
+assert part1(IN) == 2556890
