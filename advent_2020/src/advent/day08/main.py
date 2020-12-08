ef30d8e92dc743ee90de477bc0ef3c401a02d6e0
Ian Preston <17241371+ianepreston@users.noreply.github.com>
2020-12-08 15:47:48 -0700

    Compy refactor (#12)
    * move to own module
    
    * move parse to instruction class
    
    * instantiate compy with instruction list
    
    * make loops forever a class method

--- advent_2020/src/advent/day08/main.py
+++ advent_2020/src/advent/day08/main.py
@@ -2,90 +2,9 @@
 from __future__ import annotations
 
 from pathlib import Path
-from typing import List, NamedTuple, Set, Union
+from typing import List, Set
 
-
-class Instruction(NamedTuple):
-    """I'm an instruction."""
-
-    cmd: str
-    num: int
-
-
-class Compy:
-    """I'm a computer."""
-
-    def __init__(self) -> None:
-        """Instantiate with index and accumulator at 0. No instructions loaded."""
-        self.index: int = 0
-        self.accumulator: int = 0
-        self.instructions: List[Instruction] = []
-        self.run_complete: bool = False
-
-    @staticmethod
-    def _parse_instruction(line: str) -> Instruction:
-        """Turn a line of text into an Instruction.
-        
-        Parameters
-        ----------
-        line: str
-            An instruction in string form
-        
-        Returns
-        -------
-        Instruction
-            The parsed instruction
-        """
-        cmd, num = line.strip().split()
-        return Instruction(cmd, int(num))
-
-    def load_program(self, file: Union[Path, str]) -> None:
-        """Read in a text file to get a list of Instructions.
-
-        Parameters
-        ----------
-        file: Path
-            The file to load
-        """
-        with open(file, "r") as f:
-            self.instructions = [
-                self._parse_instruction(line) for line in f.readlines()
-            ]
-        self.run_complete = False
-
-    def _exec_instruction(self, instruction: Instruction) -> None:
-        """Run an instruction.
-        
-        Parameters
-        ----------
-        instruction: Instruction
-            The instruction to run
-        """
-        if instruction.cmd == "nop":
-            pass
-            self.index += 1
-        elif instruction.cmd == "acc":
-            self.accumulator += instruction.num
-            self.index += 1
-        elif instruction.cmd == "jmp":
-            if 0 <= self.index + instruction.num <= len(self.instructions):
-                self.index += instruction.num
-            else:
-                raise IndexError("Jumped out of instruction set")
-        else:
-            raise ValueError(f"Unsupported instruction: {instruction.cmd}")
-        if self.index == len(self.instructions):
-            self.run_complete = True
-
-    def step(self) -> None:
-        """Execute the next instruction."""
-        instruction: Instruction = self.instructions[self.index]
-        self._exec_instruction(instruction)
-
-    def full_run(self) -> None:
-        """Execute the program all the way through."""
-        while not self.run_complete:
-            self.step()
+from advent.compy import Compy, Instruction, instructions_from_file
 
 
 def part1(filename: str = "input.txt") -> int:
@@ -102,38 +21,11 @@ def part1(filename: str = "input.txt") -> int:
         The value of the accumulator right before a loop
     """
     indices: Set[int] = set()
-    compy: Compy = Compy()
     infile: Path = Path(__file__).resolve().parent / filename
-    compy.load_program(infile)
+    compy: Compy = Compy(instructions_from_file(infile))
     while True:
         if compy.index in indices:
             return compy.accumulator
-        else:
-            indices.add(compy.index)
-            compy.step()
-
-
-def loops_forever(instruction_list: List[Instruction]) -> bool:
-    """Check if a program is an infinite loop.
-
-    Parameters
-    ----------
-    instruction_list: List[Instruction]
-        The instruction list to check
-
-    Returns
-    -------
-    bool:
-        Whether the program will loop forever or not
-    """
-    indices: Set[int] = set()
-    compy: Compy = Compy()
-    compy.instructions = instruction_list
-    while True:
-        if compy.index in indices:
-            return True
-        elif compy.run_complete:
-            return False
         else:
             indices.add(compy.index)
             compy.step()
@@ -154,9 +46,8 @@ def part2(filename: str = "input.txt") -> int:
     """
     # Has to be an index that's encountered
     indices: Set[int] = set()
-    compy: Compy = Compy()
     infile: Path = Path(__file__).resolve().parent / filename
-    compy.load_program(infile)
+    compy: Compy = Compy(instructions_from_file(infile))
     original_instructions: List[Instruction] = compy.instructions.copy()
     while compy.index not in indices:
         indices.add(compy.index)
@@ -176,9 +67,8 @@ def part2(filename: str = "input.txt") -> int:
         swap_instruction: Instruction = Instruction(swap_cmd, candidate_instruction.num)
         swapped_instructions: List[Instruction] = original_instructions.copy()
         swapped_instructions[candidate_index] = swap_instruction
-        if not loops_forever(swapped_instructions):
+        if not Compy(swapped_instructions).loops_forever():
             break
-    compy = Compy()
-    compy.instructions = swapped_instructions
+    compy = Compy(swapped_instructions)
     compy.full_run()
     return compy.accumulator
