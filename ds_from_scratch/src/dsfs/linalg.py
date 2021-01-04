db31cd0c8da3512e5ccdd607bb2948183821dfba
Ian Preston <17241371+ianepreston@users.noreply.github.com>
2021-01-04 22:52:29 +0000

    finish off vectors

--- ds_from_scratch/src/dsfs/linalg.py
+++ ds_from_scratch/src/dsfs/linalg.py
@@ -1,4 +1,5 @@
 """Linear algebra from scratch."""
+import math
 from typing import List
 from typing import Union
 
@@ -71,3 +72,74 @@ def vector_sum(vectors: List[Vector]) -> Vector:
         raise ValueError("All vectors must be same size")
     # The i-th element of the result si the sum of every vector[i]
     return [sum(vector[i] for vector in vectors) for i in range(num_elements)]
+
+
+def scalar_multiply(c: Numeric, v: Vector) -> Vector:
+    """Multiply a vector by a scalar.
+
+    Args:
+        c: the scalar
+        v: the vector
+
+    Returns:
+        The multiplied vector
+    """
+    return [c * v_i for v_i in v]
+
+
+def vector_mean(vectors: List[Vector]) -> Vector:
+    """Compute the elementwise average.
+
+    Args:
+        vectors: the vectors to average
+
+    Returns:
+        The vector of the means of each element
+    """
+    n: int = len(vectors)
+    return scalar_multiply(1 / n, vector_sum(vectors))
+
+
+def dot(v: Vector, w: Vector) -> Numeric:
+    """Calculate the dot product.
+
+    Args:
+        v: first vector
+        w: second vector
+
+    Raises:
+        ValueError: If the vectors aren't the same length
+
+    Returns:
+        the dot product
+    """
+    if len(v) != len(w):
+        raise ValueError("Vectors must be the same size")
+    return sum(v_i * w_i for v_i, w_i in zip(v, w))
+
+
+def sum_of_squares(v: Vector) -> Numeric:
+    """Sum of vector elements squared.
+
+    Args:
+        v: the vector
+
+    Returns:
+        Sum of squares
+    """
+    return dot(v, v)
+
+
+def magnitude(v: Vector) -> float:
+    """The length of a vector."""
+    return math.sqrt(sum_of_squares(v))
+
+
+def squared_distance(v: Vector, w: Vector) -> Numeric:
+    """Computes (v_1 - w_1)^2 + ... + (v_n - w_1)^2."""
+    return sum_of_squares(subtract(v, w))
+
+
+def distance(v: Vector, w: Vector) -> float:
+    """The distance between two vectors."""
+    return magnitude(subtract(v, w))
--- ds_from_scratch/tests/test_linalg.py
+++ ds_from_scratch/tests/test_linalg.py
@@ -1,4 +1,6 @@
 """Test functionality in the linear algebra library."""
+import math
+
 from dsfs import linalg
 
 
@@ -15,3 +17,39 @@ def test_subtract() -> None:
 def test_vector_sum() -> None:
     """Test adding a list of vectors."""
     assert linalg.vector_sum([[1, 2], [3, 4], [5, 6], [7, 8]]) == [16, 20]
+
+
+def test_scalar_multiply() -> None:
+    """Test scalar multiplication."""
+    assert linalg.scalar_multiply(2, [1, 2, 3]) == [2, 4, 6]
+
+
+def test_vector_mean() -> None:
+    """Test vector mean."""
+    assert linalg.vector_mean([[1, 2], [3, 4], [5, 6]]) == [3, 4]
+
+
+def test_dot() -> None:
+    """Test dot product."""
+    assert linalg.dot([1, 2, 3], [4, 5, 6]) == 32
+
+
+def test_sum_of_squares() -> None:
+    """Test sum of squares."""
+    assert linalg.sum_of_squares([1, 2, 3]) == 14
+
+
+def test_magnitude() -> None:
+    """Test magnitude."""
+    assert linalg.magnitude([3, 4]) == 5
+
+
+def test_squared_distance() -> None:
+    """Test squared distance."""
+    assert linalg.squared_distance([1, 5], [5, 1]) == 32
+
+
+def test_distance() -> None:
+    """Test distance."""
+    # c^2 == a^2 + b^2
+    assert linalg.distance([0, 0], [3, 3]) == math.sqrt(18)
