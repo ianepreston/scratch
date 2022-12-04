a7c3c5287a70ec6df1fc781956c3d4d07ab2dfe1
Ian Preston <17241371+ianepreston@users.noreply.github.com>
2022-12-04 04:55:56 +0000

    rudimentary cli day runner

--- advent-of-code/src/advent_ipreston/__main__.py
+++ advent-of-code/src/advent_ipreston/__main__.py
@@ -1,11 +1,27 @@
 """Command-line interface."""
+import datetime as dt
+import importlib
+
 import click
 
 
 @click.command()
 @click.version_option()
-def main() -> None:
+@click.option("-y", "--year", type=str, default=None)
+@click.option("-d", "--day", type=str, default=1)
+# @click.option("-p", "--part", type=str, default=None)
+def main(year: int, day: int) -> None:
     """Ian's Advent of Code."""
+    if year is None:
+        year = dt.date.today().year
+    if day is None:
+        day = dt.date.today().day
+    module = f"advent_ipreston.year_{year}.day{str(day).zfill(2)}.puzzle"
+    daymod = importlib.import_module(module)
+
+    click.echo(f"Puzzle for {year}, day {day}")
+    click.echo(f"Part 1: {daymod.part1('puzzle.txt')}")
+    click.echo(f"Part 2: {daymod.part2('puzzle.txt')}")
 
 
 if __name__ == "__main__":
