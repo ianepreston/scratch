2ae3c64241a593519228c7ab574583e9fed410ae
Ian Preston <17241371+ianepreston@users.noreply.github.com>
2023-04-26 13:39:30 +0000

    move over to python workflow

--- dockerytdl/ytdlp.py
+++ dockerytdl/ytdlp.py
@@ -1,5 +1,28 @@
+from pathlib import Path
 from yt_dlp import YoutubeDL
 
-URLS = ['https://www.youtube.com/watch?v=BaW_jenozKc']
-with YoutubeDL() as ydl:
+
+BASE_PATH = Path("/data")
+
+with open("format.txt", "r") as f:
+	FORMAT = f.readline()
+
+
+ytdl_config = {
+	# "simulate": True,
+	"verbose": True,
+	"format": FORMAT,
+	"download_archive": BASE_PATH / "archive.log",
+	"outtmpl": "/data/%(uploader)s - %(upload_date)s - %(title)s [%(id)s].%(ext)s",
+	"writeinfojson": True,
+	"continuedl": False,
+	"overwrites": False,
+	"merge_output_format": "mkv",
+	"concurrent_fragment_downloads": 5,
+	"throttledratelimit": 100_000,
+
+}
+
+URLS = ['https://www.youtube.com/watch?v=4IJCsxeCJ6o']
+with YoutubeDL(ytdl_config) as ydl:
     ydl.download(URLS)
