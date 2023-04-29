07422ba0d365157e26d25cf669dc1fa18ceb5fda
Ian Preston <17241371+ianepreston@users.noreply.github.com>
2023-04-29 21:20:45 +0000

    move into config

--- dockerytdl/ytdlp.py
+++ dockerytdl/ytdlp.py
@@ -2,27 +2,38 @@ from pathlib import Path
 from yt_dlp import YoutubeDL
 
 
-BASE_PATH = Path("/data")
-
 with open("format.txt", "r") as f:
-	FORMAT = f.readline()
+    FORMAT = f.readline()
 
 
-ytdl_config = {
-	# "simulate": True,
-	"verbose": True,
-	"format": FORMAT,
-	"download_archive": BASE_PATH / "archive.log",
-	"outtmpl": "/data/%(uploader)s - %(upload_date)s - %(title)s [%(id)s].%(ext)s",
-	"writeinfojson": True,
-	"continuedl": False,
-	"overwrites": False,
-	"merge_output_format": "mkv",
-	"concurrent_fragment_downloads": 5,
-	"throttledratelimit": 100_000,
+def download_playlist(playlist: str, channel_folder: bool = True, delete_removed: bool = False) -> None:
+    """Download a playlist."""
+    base_path = "/data"
+    file_str: str = "%(channel)s - %(upload_date)s - %(title)s [%(id)s].%(ext)s"
+    if channel_folder:
+        folder_str = f"{base_path}/%(channel)s/%(playlist_title)s"
+    else:
+        folder_str = f"{base_path}/%(playlist_title)s"
+    ytdl_config = {
+        # "simulate": True,
+        "verbose": True,
+        "format": FORMAT,
+        # "download_archive": f"{folder_str}/archive.log",
+        "outtmpl": f"{folder_str}/{file_str}",
+        "writeinfojson": True,
+        "continuedl": False,
+        "overwrites": False,
+        "merge_output_format": "mkv",
+        "concurrent_fragment_downloads": 5,
+        "throttledratelimit": 100_000,
+    }
+    with YoutubeDL(ytdl_config) as ydl:
+        ydl.download(playlist)
 
-}
 
-URLS = ['https://www.youtube.com/watch?v=4IJCsxeCJ6o']
-with YoutubeDL(ytdl_config) as ydl:
-    ydl.download(URLS)
+if __name__ == "__main__":
+    # apaird CNC playlist
+    url = "https://www.youtube.com/watch?v=akjSc2-bZpY&list=PLZcFwaChdgSqyJ2bLlg0hyQ0vf83pPpcY"
+    download_playlist(url, channel_folder=True, delete_removed=False)
+    url = "https://www.youtube.com/watch?v=-plesrt8ZCs&list=PLQMVnqe4XbifORmvspB_Oun47H0tkbW-C"
+    download_playlist(url, channel_folder=False, delete_removed=True)
